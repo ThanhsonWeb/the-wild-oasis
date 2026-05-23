8594e75d36472cfc5bfbdd06d14bda5f66c35822
ThanhsonWeb <sondeptroi@gmail.com>
2026-05-23 09:27:59 +0700

    connecting Supabase with React App

---
+++ src/services/apiCabins.js
@@ -0,0 +1,14 @@
+import supabase from "./supabase";
+
+// step 2 : Query data -> 
+
+export async function getCabins() {
+	const { data, error } = await supabase.from("cabins").select("*");
+
+	// read cabins from database ->   give result to data
+	if (error) {
+		console.error(error.message);
+		throw new Error("Canbins could not be loaded");
+	}
+	return data;
+}
